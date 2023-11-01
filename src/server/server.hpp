9681135585b3800794246c6af3f4fc41231cdff2
Escanour-n <nourdieoukhroun@gmail.com>
2023-11-01 15:54:35 +0100

    miniserv

--- src/server/server.hpp
+++ src/server/server.hpp
@@ -4,14 +4,23 @@
 #include <sys/socket.h>
 #include <iostream>
 #include <unistd.h>
+#include <sys/types.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/select.h>
 class	ServerRun
 {
 	public :
 		ServerRun();
+		void bindConection(int Port);
+		void listenSocket();
+		void acceptRquist();
 		~ServerRun();
 	private :
 		int socketfd;
 		int newfd;
+		struct sockaddr_in addressSocketStruct;
 
 };
 #endif
