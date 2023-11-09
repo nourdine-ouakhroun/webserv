3ff8902ff4f5c6454d3741277e784c1f82553c03
Escanour-n <nourdieoukhroun@gmail.com>
2023-11-09 19:07:55 +0100

    parssing requist

--- src/server/server.cpp
+++ src/server/server.cpp
@@ -2,21 +2,28 @@
 
 ServerRun::ServerRun()
 {
-	socketfd = socket(PF_INET, SOCK_STREAM, 0);
+	
+}
+int ServerRun::Newsocket()
+{
+	int socketfd = socket(PF_INET, SOCK_STREAM, 0);
 	if(socketfd < 0)
 	{
 		std::cout << "error : socket conection not open ." << std::endl;
 		exit(1);
 	}
+	int option = 1;
+	setsockopt(socketfd, SOL_SOCKET, SO_REUSEADDR, &option, sizeof(int));
 	std::cout << socketfd << std::endl;
-		
+	return socketfd;
 }
-void ServerRun::bindConection(int Port)
+void ServerRun::bindConection(int port, int socketfd)
 {
+	struct sockaddr_in addressSocketStruct;
 	bzero(&addressSocketStruct, sizeof(addressSocketStruct));
 	addressSocketStruct.sin_family = PF_INET;
-	addressSocketStruct.sin_port = htons(Port);
-	addressSocketStruct.sin_addr.s_addr = inet_addr("10.11.1.3");
+	addressSocketStruct.sin_port = htons(port);
+	addressSocketStruct.sin_addr.s_addr = INADDR_ANY;
 
 	int bindr =  bind(socketfd,	(struct sockaddr *)&addressSocketStruct, sizeof(addressSocketStruct));
 	if(bindr < 0)
@@ -25,52 +32,7 @@ void ServerRun::bindConection(int Port)
 		exit(0);
 	}
 }
-void ServerRun::acceptRquist()
-{
-	char req[2024];
-	fd_set cur_sock, ready_sock;
-	FD_ZERO(&cur_sock);
-	FD_SET(socketfd, &cur_sock);
-
-	while(1)
-	{
-		FD_SET(socketfd, &cur_sock);
-		ready_sock = cur_sock;
-		if(select(1024, &ready_sock, NULL, NULL, NULL) < 0)
-		{
-			std::cout << "error : select" << std::endl;
-			exit(0);
-		}
-		for(int i = 0; i < FD_SETSIZE; i++)
-		{
-			if(FD_ISSET(i, &ready_sock))
-			{
-				if(i == socketfd)
-				{
-					newfd = accept(socketfd, (struct sockaddr *)NULL, NULL);
-					FD_SET(newfd, &cur_sock);
-					break;
-				}
-				else
-				{
-					int bytes = read(i, req, 2024);
-					if(bytes < 0)
-					{
-						std::cout << "error : read failed ." << std::endl;
-						exit(0);
-					}
-					const char *str = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<h1>hello world</h1>";
-					write(i,str,strlen(str));
-					std::cout << req << std::endl;
-					FD_CLR(i, &cur_sock);
-					close(i);
-				}
-			}
-		}
-	}
-}
-
-void ServerRun::listenSocket()
+void ServerRun::listenSocket(int socketfd)
 {
 	int lestenr = listen(socketfd, 10);
 	if(lestenr < 0)
@@ -81,6 +43,5 @@ void ServerRun::listenSocket()
 }
 ServerRun::~ServerRun()
 {
-	close(socketfd);
-	close(newfd);
+	//close(socketfd);
 }
