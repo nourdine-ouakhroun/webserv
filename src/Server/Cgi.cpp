10cede5d7d56e446d048553e7c811f1678dc2c3d
Escanour-n <nourdieoukhroun@gmail.com>
2023-11-14 21:01:44 +0100

    make fclean

--- src/Server/Cgi.cpp
+++ src/Server/Cgi.cpp
@@ -8,51 +8,36 @@ std::string Cgi::HandelScript()
 {
     int forkValeu = 0;
     char beffur[2024] = {0};
-	if(this->PathScript.substr(PathScript.find('.') + 1) == "c++")
+	if(this->PathScript.substr(PathScript.find('.') + 1) == "cpp")
     {
-        const char *argv[] = {"/usr/bin/c++", (char*)PathScript.c_str(),NULL};
+        std::string cmd("/usr/bin/c++ ");
+        cmd.append(PathScript).append(" && ./a.out");
+        const char *argv[] = {"/bin/bash", "-c", (char*)cmd.c_str(),NULL};
+        int fd[2];
+        pipe(fd);
+        std::cout << cmd << std::endl;
         forkValeu = fork();
         if(forkValeu == 0)
         {
+            dup2(fd[1],STDOUT_FILENO);
+            close(fd[0]);
             if(execve(argv[0], (char *const *)argv, NULL) < 0)
             {
-                std::cout << "hello" << std::endl;
+                std::cout << "execve filed" << std::endl;
                 exit(0);
             }
         }
         else
         {
             wait(0);
-            int fd[2];
-            pipe(fd);
-            forkValeu = fork();
-            if(forkValeu == 0)
-            {
-                dup2(fd[1],STDOUT_FILENO);
-                close(fd[0]);
-                if(execve("./a.out", NULL, NULL) < 0)
-                {
-                    std::cout << "hello" << std::endl;
-                    exit(0);
-                }
-            }
-            else
-            {
-                wait(0);
-                forkValeu = fork();
-                const char *argv[] = {"/bin/rm", "./a.out", NULL};
-                if(forkValeu == 0)
-                {
-                    if(execve(argv[0], (char *const *) argv, NULL) < 0)
-                    {
-                        std::cout << "hello" << std::endl;
-                        exit(0);
-                    }
-                }
-                read(fd[0], beffur, 2023);
-                close(fd[0]);
-                close(fd[1]);
-            }
+            // if(unlink("./a.out") < 0)
+            // {
+            //     std::cout << "error" << std::endl;
+            //     exit(1);
+            // }
+            read(fd[0], beffur, 2023);
+            close(fd[0]);
+            close(fd[1]);
         }
     }
     return beffur;
