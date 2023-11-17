187b45bd7f77aff749bf255f1dddf31543ebb6b3
mehdisalim <mehdisalimofficial2000@gmail.com>
2023-11-17 20:57:34 +0100

    add html project for testing my web server.

---
+++ src/Includes/Request.hpp
@@ -0,0 +1,15 @@
+#ifndef REQUEST_HPP
+#define REQUEST_HPP
+
+#include "String.hpp"
+
+class   Request
+{
+    public :
+        Request( void );
+        Request(String _request);
+        // Request(const Request& copy);
+
+};
+
+#endif
