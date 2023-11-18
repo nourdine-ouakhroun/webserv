188dba3e4509c487c747610ce25331326eafd3eb
m-zeroual <mohammedzeroual200@gmail.com>
2023-11-18 09:55:40 +0100

    I start from this commit!

--- src/server/server.hpp
+++ src/server/server.hpp
@@ -8,7 +8,7 @@
 #include <unistd.h>
 #define SA 		struct sockaddr
 #define SA_IN 	struct sockaddr_in
-#define PORT 8080
+#define PORT 80
 #define SIZE 2048
 
 class	Server
@@ -16,39 +16,10 @@ class	Server
 	public :
 		Server();
 		~Server();
-		// int	errorExit(int status, std::string errorMsg)
-		// {
-		// 	if (status < 0)
-		// 	{
-		// 		std::cerr << errorMsg << std::endl;
-		// 		exit (1);
-		// 	}
-		// 	return (status);
-		// }
-		// std::string readRequest(int &clientFd)
-		// {
-		// 	std::string	data;
-		// 	int			byte = 0;
-		// 	int			readbyte = 1;
-		// 	char		request[readbyte + 1];
-
-		// 	while(1)
-		// 	{
-		// 		byte = errorExit(read(clientFd, request, readbyte), "Error : read Faile!.\n");
-		// 		request[byte] = 0;
-		// 		data += request;
-		// 		if(data.find("\r\n\r\n") != std::string::npos)
-		// 			break;
-		// 	}
-		// 	return (data);
-		// }
 
 	private :
 		int			servFd;
 		int			clientFd;
-		// char 		request[SIZE];
-
 		SA_IN		servAddr;
-
 };
 #endif
