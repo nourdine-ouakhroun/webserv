0194b37f170418effd02982f62c3fb87e69c6bdb
m-zeroual <mohammed.zeroual.mz@gmail.com>
2023-11-21 15:04:39 +0100

    save

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -1,14 +1,6 @@
 #include "server.hpp"
-#include "Parser.hpp"
-#include "ServerData.hpp"
-#include "Checker.hpp"
 
 int main()
 {
-	Parser	parser("configurations/default.conf");
-	Checker checker(parser.getServers());
-	checker.fullCheck();
-	ServerData servers(parser.getServers());
-	std::vector<ServerModel> serv = servers.getAllServers();
-	Server s;
+	Server	s;
 }
