c2b857a97f46bea0480cbd4cca49ae8173ffddc6
Escanour-n <nourdieoukhroun@gmail.com>
2023-11-28 18:41:59 +0100

    poll

--- src/Server/Server.cpp
+++ src/Server/Server.cpp
@@ -1,32 +1,28 @@
 #include"Server.hpp"
 
-Server::Server(ServerData	srvers)
-{
-	this->servers = srvers;
-}
-void	Server::setSocket(int fd)
-{
-	socket(fd, )
-}
 
-void Server::runAllServers()
+void	Server::bindSocket(int fd, int port)
 {
-	std::vector <ServerModel> allservers =  servers.getAllServers();
-	for (size_t i = 0; i < allservers.size(); i++)
+	S_address socket_info;
+	socket_info.sin_family = PF_INET;
+	socket_info.sin_port = htons(port);
+	socket_info.sin_addr.s_addr = INADDR_ANY;
+	if(bind(fd, (const sockaddr*)&socket_info, sizeof(socket_info)) == -1)
 	{
-		std::vector <Data> ports = allservers[i].getData("listen");
-		for (size_t j = 0; j < ports.size(); j++)
-		{
-			fdports.push_back(static_cast<int>(strtol(ports[i].getValue().c_str(),NULL, 10)));
-		}
+		close(fd);
+		throw std::runtime_error("bind : can't Bind this Port");
 	}
-	for(size_t i = 0; i < fdports.size();i++)
-	{
-		setSocket(fdports[i]);
 
-	}
 }
-
-Server::~Server()
+void	Server::listenPort(int Socketfd)
 {
+	if(listen(Socketfd, BACKLOG)  == -1)
+		throw std::runtime_error("listen : can't listen");
+}
+int		Server::setSocket()
+{
+	int fdSocket = socket(PF_INET, SOCK_STREAM, 0);
+	if(fdSocket < -1)
+		throw std::runtime_error("socket : can't open the file conection");
+	return fdSocket;
 }
