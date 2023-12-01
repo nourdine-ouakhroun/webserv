afffddbbd0f2bbb974d5aa8513cd8b6905836db8
mehdisalim <mehdisalimofficial2000@gmail.com>
2023-12-01 11:14:16 +0100

    add responseheader class

---
+++ src/Utils/ResponseHeader.cpp
@@ -0,0 +1,72 @@
+#include "ResponseHeader.hpp"
+
+ResponseHeader::ResponseHeader( void )
+{
+    this->_protocol    = "HTTP/1.1";
+    this->_status      = "200 OK";
+    this->_server      = "webserv";
+}
+
+ResponseHeader& ResponseHeader::protocol(const String& _protocol)
+{
+    this->_protocol = _protocol;
+    return (*this);
+}
+
+ResponseHeader& ResponseHeader::status(const String& _status)
+{
+    this->_status = _status;
+    return (*this);
+}
+
+ResponseHeader& ResponseHeader::server(const String& _server)
+{
+    this->_server = _server;
+    return (*this);
+}
+
+
+ResponseHeader& ResponseHeader::contentType(const String& _contentType)
+{
+    this->_contentType = _contentType;
+    return (*this);
+}
+
+ResponseHeader& ResponseHeader::contentLength(const String& _contentLength)
+{
+    this->_contentLength = _contentLength;
+    return (*this);
+}
+
+ResponseHeader& ResponseHeader::location(const String& _location)
+{
+    this->_location = _location;
+    return (*this);
+}
+
+ResponseHeader& ResponseHeader::connection(const String& _connection)
+{
+    this->_connection = _connection;
+    return (*this);
+}
+
+ResponseHeader& ResponseHeader::body(const String& _body)
+{
+    this->_body = _body;
+    return (*this);
+}
+
+String  ResponseHeader::toString( void )
+{
+    String response(this->_protocol + " " + this->_status + "\r\n");
+    response.append("Server: " + this->_server + "\r\n");
+    if (!this->_contentLength.empty())
+        response.append("Content-Type: " + this->_contentType + "\r\n");
+    if (!this->_contentLength.empty())
+        response.append("Content-Length: " + this->_contentLength + "\r\n");
+    if (!this->_connection.empty())
+        response.append("Connection: " + this->_connection + "\r\n");
+    response.append("\r\n");
+    response.append(this->_body);
+    return (response);
+}
---
+++ src/configurationTools.cpp
@@ -0,0 +1,79 @@
+#include "Parser.hpp"
+#include "Checker.hpp"
+#include "Server.hpp"
+#include <dirent.h>
+
+String  getContentFile(String file)
+{
+    std::fstream new_file;
+    
+    new_file.open(file, std::ios::in);
+    if (new_file.is_open() == false)
+        return ("");
+    String  sa;
+    String  content;
+    while (std::getline(new_file, sa))
+        content.append(sa).append("\n");
+    new_file.close();
+    return content;
+}
+
+String	getRootPath(String	root, String path)
+{
+	String file(root);
+	file.append(path).append((file.end() - 1)[0] == '/' ? "" : "/");
+	return (file);
+}
+
+String	getAliasPath(String	aliasPath)
+{
+	aliasPath.rightTrim("/");
+	return (aliasPath.append("/"));
+}
+
+String	tryFiles(const std::vector<String>& files, const String& path)
+{
+	String value;
+	for (size_t i = 0; i < files.size() - 1; i++)
+	{
+		String tmp(path);
+		tmp.append(files.at(i));
+		value = readFile(tmp);
+		if (value.length() != 0)
+			return (value);
+	}
+	return (value);
+}
+
+String	getFileContent(const std::vector<String>& indexes, const String& path)
+{
+	String value;
+	for (size_t i = 0; i < indexes.size(); i++)
+	{
+		String tmp(path);
+		tmp.append(indexes.at(i));
+		value = readFile(tmp);
+		if (value.length() != 0)
+			return (value);
+	}
+	return (value);
+}
+
+String	getDirectoryContent(const String& dirname, String path)
+{
+	DIR	*dir = opendir(dirname.c_str());
+	if (!dir)
+		return "";
+	String body("<h1>Index of ");
+	body.append(path).append("</h1><hr><pre>");
+	struct dirent *dirp;
+	path.trim("/");
+	while ((dirp = readdir(dir)) != NULL)
+		body.append("<a href=\"").append(path)
+			.append("/").append(dirp->d_name)
+			.append("\">").append(dirp->d_name)
+			.append("</a><br>");
+	body.append("</pre><hr>");
+	closedir(dir);
+	return (body);
+}
