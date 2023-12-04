8babbecd01cdc5be45b8e656f9f034ffe1623e46
Escanour-n <nourdieoukhroun@gmail.com>
2023-12-05 00:57:12 +0100

    file depandence

--- src/Server/ManageServers.cpp
+++ src/Server/ManageServers.cpp
@@ -51,55 +51,60 @@ void erase(std::vector<FileDepandenc> & struct_fdsS, size_t j)
 	}
 	struct_fdsS = returnFds;
 }
-// static size_t k;
 
 std::string readRequist(FileDepandenc &file)
 {
 	ssize_t		bytes;
-	std::string	boundary;
+	String	boundary;
 	bytes = 0;
 	char		req[2025];
-	while(true)
-	{
-		memset(req, 0, 2025);
-		bytes = recv(file.fdpoll.fd, req, 2024, 0);
-		// if(k >= 5)
-		// 	exit(1);
-		if(bytes <= 0)
-			break;
-		std::string reqtmp = req;
-		reqtmp.insert(0, file.rest);
-		size_t pos;
-		// std::cout << "(" << req << ")"<< std::endl;
-		pos = reqtmp.find_last_of("\r\n");
-		if(pos != SIZE_T_MAX)
-			pos = reqtmp.size() - 1;
-		std::string filtred = reqtmp.substr(0 , pos + 1);
-		std::string resttmp = reqtmp.substr(pos + 1 , reqtmp.size() - pos + 1);
-		std::cout << "[" << resttmp << "]"<< std::endl;
-		file.requist.append(filtred, file.requist.size(), filtred.size());
-		file.rest = resttmp;
 
-		if(file.getStatus() == 0 && file.boundery.empty() && (pos = filtred.find("boundary=", 0)) != SIZE_T_MAX)
-		{
-			file.boundery = filtred.substr(pos + 9, filtred.find("\r\n", (pos + 9)) - (pos + 9));
-			file.setStatus(1);
-		}
-		// if(!file.header.empty() && (pos = file.getRequist().find("\r\n\r\n", 0)) != SIZE_T_MAX)
+	memset(req, 0, 2025);
+	bytes = recv(file.fdpoll.fd, req, 2024, 0);
+	if(bytes > 0)
+	{
+		file.lenght += (size_t)bytes;
+		String reqtmp = req;
+		reqtmp.insert(0, file.rest);
+
+		size_t pos;
+		pos = reqtmp.memStringSearchLast("\r\n", (size_t)bytes + file.rest.size());
+		if(pos == SIZE_T_MAX)
+			pos = reqtmp.size() - 1;
+		else
+			pos++;
+		String filtred = reqtmp.memSubstr(0 , pos);
+		std::cout << filtred << std::endl;
+		// exit(1);
+		// file.rest = resttmp;
+		// if(k == 3)
 		// {
-		// 	std::string content = file.getRequist().substr(0 , pos);
-		// 	if((pos = content.find("filename=")) != SIZE_T_MAX)
-		// 		std::cout << content.substr(pos + 9, content.find("\r\n" , pos) - (pos + 9));
+		// 	for (size_t i = 0; i < (size_t)bytes; i++)
+		// 	{
+		// 		std::cout << (int)reqtmp[i] << std::endl;
+		// 	}
+		// 	std::cout << reqtmp << " : " << bytes << " : " <<  reqtmp.size() << std::endl;
 		// 	exit(1);
 		// }
-		if(!file.boundery.empty() && file.getStatus() && filtred.find( "--" + file.boundery + "--" ) != SIZE_T_MAX)
-		{
-			file.setStatus(0);
-		}
+		// k++;
+		// size_t size = file.requist.size();
+		// if(size != 0)
+		// 	size--;
+		// file.requist.append(filtred, 0, filtred.size());
+		// // if()
+		// if(file.getStatus() == 0 && file.boundery.empty() && (pos = filtred.find("boundary=", 0)) != SIZE_T_MAX)
+		// {
+		// 	file.boundery = filtred.substr(pos + 9, filtred.find("\r\n", (pos + 9)) - (pos + 9));
+		// 	file.setStatus(1);
+		// }
+		// if(!file.boundery.empty() && file.getStatus() && filtred.find( "--" + file.boundery + "--" ) != SIZE_T_MAX)
+		// {
+		// 	file.setStatus(0);
+		// }
 	}
 	if(file.getStatus() || file.requist.empty())
 		throw std::runtime_error("");
-
+	std::cout << file.requist << std::endl;
 	return "HTTP/1.1 200 OK\r\n\r\n <h1> hello </h1>";
 }
 void ManageServers::handler(std::vector<FileDepandenc> &working, std::vector<FileDepandenc> &master, size_t i)
