8e7c8b11f41ba1658a78ad9c5d8ecc424082e3d4
mehdisalim <mehdisalimofficial2000@gmail.com>
2024-02-29 11:09:22 +0100

    handling default server in config file.

--- src/Utils/ServerData.cpp
+++ src/Utils/ServerData.cpp
@@ -74,14 +74,14 @@ vector<ServerPattern>	ServerData::getServersByIpAndPort(const vector<ServerPatte
 	{
 		vector<Data> value = iterBegin->getData("listen");
 		for (size_t i = 0; i < value.size(); i++)
-			if (!value[i].getValue().compare(strHost))
+			if (!value[i].getValue().split()[0].compare(strHost))
 				serv.push_back(*iterBegin);
 		iterBegin++;
 	}
 	return (serv);
 }
 
-const ServerPattern&	ServerData::getDefaultServer(const vector<ServerPattern>& servers)
+ServerPattern	ServerData::getDefaultServer(const vector<ServerPattern>& servers)
 {
 	vector<ServerPattern>	serv;
 	vector<ServerPattern>::const_iterator iterBegin = servers.begin();
@@ -98,14 +98,14 @@ const ServerPattern&	ServerData::getDefaultServer(const vector<ServerPattern>& s
 		}
 		iterBegin++;
 	}
-	if (serv.size() > 1)
-		throw (ServerException("Duplicate default server."));
-	else if (serv.size() == 0)
+	// if (serv.size() > 1)
+	// 	Logger::warn(cout, "Duplicate default server.", "");
+	if (serv.size() == 0)
 		serv.push_back(servers.front());
 	return (serv.front());
 }
 
-vector<ServerPattern> ServerData::getServer(const ServerData &servers, __unused const String &ClientAddress, String strHost)
+vector<ServerPattern>	ServerData::getServer(const ServerData& servers,  const String& ClientAddress, String strHost)
 {
 	vector<ServerPattern>	srvs = servers.getAllServers();
 	srvs = ServerData::getServersByIpAndPort(srvs, ClientAddress);
