6c83533768deac793a856c8a11a365aea9118409
mehdisalim <mehdisalimofficial2000@gmail.com>
2024-02-29 11:09:22 +0100

    handling default server in config file.

--- src/Includes/ServerData.hpp
+++ src/Includes/ServerData.hpp
@@ -5,6 +5,7 @@
 #include "ServerException.hpp"
 #include <algorithm>
 
+
 #define	ERROR_404	"<h1 style=\"text-align: center;\">404 Page not found.</h1>"
 #define	ERROR_403	"<h1 style=\"text-align: center;\">403 Forbidden.</h1>"
 
@@ -22,10 +23,11 @@ class ServerData
 		void	displayServers( void );
 		static vector<ServerPattern>	getServersByServerName(const vector<ServerPattern>& servers, const String& serverName);
 		static vector<ServerPattern>	getServersByIpAndPort(const vector<ServerPattern>& servers, String strHost);
-		static const ServerPattern&	getDefaultServer(const vector<ServerPattern>& servers);
+		static ServerPattern			getDefaultServer(const vector<ServerPattern>& servers);
 		const vector<ServerPattern>&	getAllServers() const;
 
-		static vector<ServerPattern> getServer(const ServerData &servers, const String &ClientAddress, String strHost);
+		static vector<ServerPattern>	getServer(const ServerData& servers, const String& ClientAddress, String strHost);
+
 };
 
 #endif
