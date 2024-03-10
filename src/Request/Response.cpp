02e31d869153c55ac9f2e543f8212de910e1ec3a
Mohammed Zeroual <mzeroual@c1r2p7.1337.ma>
2024-03-10 19:36:12 +0000

    all is good

--- src/Request/Response.cpp
+++ src/Request/Response.cpp
@@ -2,19 +2,6 @@
 #include <cmath>
 #include <signal.h>
 
-void removeSlash(string &path) {
-	size_t i = 0;
-	string str;
-	while (path[i])
-	{
-		if ((path[i] == '/' && path[i + 1] != '/') || path[i] != '/')
-			str += path[i];
-
-		i++;
-	}
-	path = str;
-}
-
 int Response::isDirectory(const std::string& path)
 {
 	struct stat statbuf;
@@ -59,7 +46,6 @@ string Response::runScript(vector<String> args, string fileName)
 					&fileName[0],
 					NULL
 				};
-
 				string str[11] = {
 					("REDIRECT_STATUS=200\0"),
 					("HTTP_CONNECTION=" + request.header("Connection") + "\0"),
@@ -73,7 +59,6 @@ string Response::runScript(vector<String> args, string fileName)
 					("QUERY_STRING=" + request.getQuery() + "\0"),
 					("REQUEST_METHOD=" + request.getMethod() + "\0")
 				};
-
 				char	*envp[12];
 				size_t i = 0;
 				for ( i = 0 ; i < 11; i++)
@@ -103,9 +88,6 @@ string Response::runScript(vector<String> args, string fileName)
             		if (waitpid(pid, &status, WNOHANG) == pid) {
                 		break;
             		}
-					// cout << "start_time  : " <<  start_time << endl;
-					// cout << "current_time: " <<  (double(time(NULL) - start_time)) << endl;
-
             		double current_time = double(time(NULL) - start_time);
             		if (current_time > timeout_seconds) {
                 		kill(pid, SIGKILL);
@@ -114,13 +96,11 @@ string Response::runScript(vector<String> args, string fileName)
 					}
 					usleep(100000); // 100 milliseconds
             	}
-
 				if (WIFEXITED(status)) {
 					if (WEXITSTATUS(status) != 0) {
 						cout << "status: " << WEXITSTATUS(status) << endl;
 						// throw 500;
 					}
-
 				}
 				char res[200];
 				bzero(res, 200);
@@ -311,7 +291,6 @@ void Response::isRedirected() {
 	if (!location.empty()) {
 		vector<Data> redirection = location.getData("return");
 		if (redirection.size()) {
-			cout << "redirection: " << redirection[0].getValue() << endl;
 			vector<string> sp = split(redirection[0].getValue(), " ");
 			for (size_t i = 0; i < sp.size(); i++)
 			{
@@ -338,9 +317,6 @@ void Response::isMethodAllowed() {
 }
 void Response::whichMethod() {
 	string path = request.getPath();
-	cout << "path: " << path << endl;
-	removeSlash(path);
-	cout << "path: " << path << endl;
 	string root = getRoot();
 	string alias = getAlias();
 
@@ -368,69 +344,6 @@ void Response::whichMethod() {
 		DeleteMethod(pathToServe);
 	}
 }
-
-// geters configFile
-// std::string Response::getFullPath()
-// {
-// 	bool isFound = false;
-// 	std::string path = request.getPath();
-// 	std::string root;
-// 	std::string fullPath;
-
-// 	vector<String> pathss;
-// 	ServerPattern::getAllLocationPath(server.getLocations(), pathss);
-// 	vector<String> newPaths;
-// 	String test(path);
-// 	test.rightTrim("/");
-// 	for (size_t i = 0; i < pathss.size(); i++)
-// 	{
-// 		if (!strncmp(pathss[i].c_str(), test.c_str(), pathss[i].size()) && pathss[i].size() == test.size())
-// 		{
-// 			newPaths.clear();
-// 			newPaths.push_back(pathss[i]);
-// 			break;
-// 		}
-// 		if (!strncmp(pathss[i].c_str(), test.c_str(), pathss[i].size()))
-// 			newPaths.push_back(pathss[i]);
-// 	}
-// 	vector<String>::iterator it = max_element(newPaths.begin(), newPaths.end());
-// 	if (it == newPaths.end())
-// 		throw 404;
-// 	location = server.getLocationByPath(server.getLocations(), newPaths[0]);
-// 	cout << "hello => " << location.getPath().empty() << endl;
-// 	if (!location.getPath().empty())
-// 	{
-// 		std::vector<Data> vRoot = location.getData("root");
-// 		if (!vRoot.empty())
-// 		{
-// 			root = vRoot[0].getValue();
-// 			if (isDirectory(root + request.getPath()) == 1 && (root + request.getPath()).back() != '/')
-// 				throw 301;
-// 		}
-// 		if (!(isDirectory(root + path) == 1))
-// 		{
-// 			return (root + path);
-// 		}
-// 		std::vector<Data> vIndex = location.getData("index");
-// 		if (!vIndex.empty())
-// 		{
-// 			for (size_t i = 0; (i < vIndex.size() && !isFound); i++)
-// 			{
-// 				std::vector<std::string> filename = split(vIndex[i].getValue(), " ");
-// 				for (size_t j = 0; (j < filename.size() && !isFound); j++)
-// 				{
-// 					std::string fileExist = root + path + filename[j];
-// 					if (access(fileExist.c_str(), O_RDONLY) != -1)
-// 					{
-// 						fullPath = fileExist;
-// 						isFound = true;
-// 					}
-// 				}
-// 			}
-// 		}
-// 	}
-// 	return (fullPath);
-// }
 
 std::string Response::getErrorFile(int statusCode) const
 {
@@ -499,8 +412,6 @@ string Response::getLocationPath() {
 	return (location.getPath());
 }
 
-
-
 string Response::isUpload() {
 	vector<Data> uploadFile = location.getData("upload_dir");
 	if ( !uploadFile.empty())
@@ -513,37 +424,16 @@ string Response::isUpload() {
 const string& Response::getRedirection() const {
 	return (_redirection);
 }
-void Response::setRedirection(const string& redirection)
-{
+void Response::setRedirection(const string& redirection) {
 	this->_redirection = redirection;
 }
 const map<int, string>& Response::getStatusMessage() const {
 	return (statusMessage);
 }
 
+
+
 void Response::GetMethod(const string& pathToServe) {
-
-	// removeSlash(path);
-	// cout << "path: " << path << endl;
-	
-	// cout << "path: " << path << endl;
-	// string root = getRoot();
-	// string alias = getAlias();
-
-	// if (!alias.empty()) {
-	// 	string locationPath = location.getPath();
-	// 	// cout << "locationPath: " << locationPath << endl;
-	// 	string newPath;
-	// 	size_t pos = 0;
-	// 	if ((pos = path.find(locationPath)) != string::npos)
-	// 		newPath = path.substr(pos + locationPath.length(), path.length() - pos + locationPath.length());
-	// 	// cout << newPath << endl;
-	// 	pathToServe = alias + newPath;  
-	// }
-	// else
-	// 	pathToServe = root + request.getPath();
-
-	// string path = request.getPath();
 	string index;
 	if (isDirectory(pathToServe) == 1) {
 		// is Directory
@@ -603,22 +493,9 @@ void Response::GetMethod(const string& pathToServe) {
 	throw 200;
 }
 void Response::PostMethod(const string& pathToServe) {
-	// string path = request.getPath();
-	// string root = getRoot();
-	// string alias = getAlias();
-
-	// if (!alias.empty()) {
-	// 	string locationPath = location.getPath();
-	// 	string newPath;
-	// 	size_t pos = 0;
-	// 	if ((pos = path.find(locationPath)) != string::npos)
-	// 		newPath = path.substr(pos + locationPath.length(), path.length() - pos + locationPath.length());
-	// 	cout << newPath << endl;
-	// 	pathToServe = alias + newPath;
-	// }
-	// else
-	// 	pathToServe = root + request.getPath();
 	string index;
+	cout << "pathToServe: " << pathToServe << endl;
+
 	if (isDirectory(pathToServe) == 1) {
 		// is Directory
 		if (request.getPath() != "/" && pathToServe[pathToServe.size() - 1] != '/') {
@@ -633,14 +510,27 @@ void Response::PostMethod(const string& pathToServe) {
 			else if (isCgi()) // if location have cgi
 				// run cgi here
 				setResponse(runScript(location.getData("cgi")[0].getValue().split(' ') , pathToServe + index));
+			else if (!isUpload().empty()) {
+				string uploadDir = isUpload();
+				if (uploadDir.back() != '/')
+					uploadDir.append("/");
+				
+				vector<pair<string, string> > upload = request.getUploads();
+				for (size_t i = 0; i < upload.size(); i++)
+				{
+					if (!upload[i].first.empty()) {
+						ofstream up(uploadDir + upload[i].first);
+						up << upload[i].second;
+						up.close();
+					}
+				}
+			}
+
 		}
 		else
 			throw 403;
 	}
 	else if (isFile(pathToServe) == 1) {
-		// is File
-		// if (pathToServe == alias)
-		// 	throw 500;
 		size_t pos;
 		string extention;
 		if ((pos = pathToServe.rfind(".")) != string::npos)
@@ -648,6 +538,22 @@ void Response::PostMethod(const string& pathToServe) {
 		if (isCgi() && !extention.empty() && extention == location.getData("cgi")[0].getValue().split(' ')[1])
 			// run cgi here
 			setResponse(runScript(location.getData("cgi")[0].getValue().split(' ') , pathToServe));
+		else if (!isUpload().empty()) {
+			string uploadDir = isUpload();
+			if (uploadDir.back() != '/')
+				uploadDir.append("/");
+			if (!opendir(uploadDir.c_str()))
+				throw 500;
+			vector<pair<string, string> > upload = request.getUploads();
+			for (size_t i = 0; i < upload.size(); i++)
+			{
+				if (!upload[i].first.empty()) {
+					ofstream up(uploadDir + upload[i].first);
+					up << upload[i].second;
+					up.close();
+				}
+			}
+		}
 		else {
 			if (access((request.getPath() + index).c_str(), O_RDONLY) != -1)
 				throw 404;
@@ -660,22 +566,6 @@ void Response::PostMethod(const string& pathToServe) {
 	throw 200;
 }
 void Response::DeleteMethod(const string& pathToServe) {
-	// cout << "Delete" << endl;
-	// string path = request.getPath();
-	// string root = getRoot();
-	// string alias = getAlias();
-
-	// if (!alias.empty()) {
-	// 	string locationPath = location.getPath();
-	// 	string newPath;
-	// 	size_t pos = 0;
-	// 	if ((pos = path.find(locationPath)) != string::npos)
-	// 		newPath = path.substr(pos + locationPath.length(), path.length() - pos + locationPath.length());
-	// 	cout << newPath << endl;
-	// 	pathToServe = alias + newPath;
-	// }
-	// else
-	// 	pathToServe = root + request.getPath();
 	cout << "path to delete: " << pathToServe << endl;
 	string index;
 	if (isDirectory(pathToServe) == 1) {
